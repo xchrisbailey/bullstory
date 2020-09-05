c06be22049f3e1913b502c01c1aff9f1f6337266
Chris Bailey <chris@ghostly.io>
2020-09-05 12:08:50 -0400

    move pages to src folder

--- src/pages/api/hello.tsx
+++ src/pages/api/hello.tsx
@@ -1,5 +1,3 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-
 import { NextApiRequest, NextApiResponse } from 'next'
 
 const handler = (req: NextApiRequest, res: NextApiResponse) => {
